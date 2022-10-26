3c438a4601eba4dbda697f7e93a2e3cc8610ba71
Dayona <dayonaart@gmail.com>
2022-10-26 13:32:09 +0700

    first commit

---
+++ src/main/java/dayona/zoloz/ZolozApplication.java
@@ -0,0 +1,13 @@
+package dayona.zoloz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZolozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZolozApplication.class, args);
+	}
+
+}
